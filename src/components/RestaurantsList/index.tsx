b6569eb1652a67cf18f1f4b89ed0a544d9f6853b
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-08-30 01:24:34 -0300

    feat: creating functionality to fetch data from the API

--- src/components/RestaurantsList/index.tsx
+++ src/components/RestaurantsList/index.tsx
@@ -1,29 +1,41 @@
 import Restaurant from 'components/Restaurant';
 import { RestaurantsListContainer } from './styles';
 import { IRestaurant } from 'interfaces/IRestaurant';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+import useFetchData from 'global/hooks/useFetchData';
 
 const RestaurantsList = () => {
-  const [restaurant, setRestaurant] = useState<IRestaurant[]>([]);
+  // const [restaurant, setRestaurant] = useState<IRestaurant[]>([]);
+  const {
+    data: restaurants,
+    loading,
+    error,
+  } = useFetchData<IRestaurant[]>(
+    'https://fake-api-tau.vercel.app/api/efood/restaurantes'
+  );
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          'https://fake-api-tau.vercel.app/api/efood/restaurantes'
-        );
-        const data = await response.json();
-        setRestaurant(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  if (!restaurants) return <p>No data available</p>;
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         'https://fake-api-tau.vercel.app/api/efood/restaurantes'
+  //       );
+  //       const data = await response.json();
+  //       setRestaurant(data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <RestaurantsListContainer>
-      {restaurant.map(
+      {restaurants.map(
         ({ id, titulo, capa, tipo, avaliacao, destacado, descricao }) => (
           <Restaurant
             key={id}
