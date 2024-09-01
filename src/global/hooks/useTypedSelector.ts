3f11bdc7eafa2ef28eb639b42f407b22924dc892
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-09-01 04:45:06 -0300

    fix: removing hook useTypedSelector

--- src/global/hooks/useTypedSelector.ts
+++
@@ -1,6 +0,0 @@
-import { useSelector, TypedUseSelectorHook } from 'react-redux';
-import { IRootState } from 'interfaces/IRootState';
-
-const useTypedSelector: TypedUseSelectorHook<IRootState> = useSelector;
-
-export default useTypedSelector;
