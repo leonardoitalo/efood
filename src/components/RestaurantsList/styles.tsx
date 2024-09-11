2e874bc625dbf4f4e98bb331e6f8d640aeab808d
Leonardo <leonardoitaloapbr@gmail.com>
2024-09-11 17:07:06 -0300

    fix: adjusting layout

--- src/components/RestaurantsList/styles.tsx
+++ src/components/RestaurantsList/styles.tsx
@@ -2,7 +2,19 @@ import { colors } from 'global/variablesCss';
 import styled from 'styled-components';
 
 export const RestaurantsListContainer = styled.main`
-  padding: 120px 12.5vw;
+  display: grid;
+  justify-items: center;
+  grid-template-columns: repeat(2, 1fr); // 2 colunas com tamanho igual
+  row-gap: 48px; // Espaço entre os itens (pode ajustar o valor como preferir)
+  width: 100%;
+  padding: 120px 23.5vw;
+
+  @media (max-width: 768px) {
+    // Ajuste para telas menores
+    grid-template-columns: 1fr; // Exibe 1 item por linha em telas menores
+  }
+
+  /* padding: 120px 12.5vw;
   background-color: ${colors.beige};
   display: flex;
   flex-wrap: wrap;
@@ -10,4 +22,9 @@ export const RestaurantsListContainer = styled.main`
   align-items: center;
   justify-content: center;
   gap: 48px 80px;
+
+  & > * {
+    flex-basis: calc(50% - 80px);
+    max-width: calc(50% - 80px);
+  } */
 `;
