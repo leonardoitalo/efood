03a04e5c16dcbd6c90bcb4d9772e2836514ee155
Leonardo <leonardoitaloapbr@gmail.com>
2024-09-13 17:34:04 -0300

    fix: adjusting layout

--- src/components/RestaurantsList/styles.tsx
+++ src/components/RestaurantsList/styles.tsx
@@ -4,13 +4,15 @@ import styled from 'styled-components';
 export const RestaurantsListContainer = styled.main`
   display: grid;
   justify-items: center;
-  grid-template-columns: repeat(2, 1fr); // 2 colunas com tamanho igual
-  row-gap: 4vw; // Espaço entre os itens (pode ajustar o valor como preferir)
-  width: 100vw;
+  grid-template-columns: repeat(2, minmax(500px, 0.1fr));
+  row-gap: 48px; // Espaço entre os itens (pode ajustar o valor como preferir)
+  column-gap: 80px;
+  width: 100%;
   padding: 120px 12.5vw;
   background-color: ${colors.beige};
+  justify-content: center;
 
-  @media (max-width: 768px) {
+  @media (max-width: 900px) {
     // Ajuste para telas menores
     grid-template-columns: 1fr; // Exibe 1 item por linha em telas menores
   }
